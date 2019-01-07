d26b9dc45d6c3bbccf4681f801cf13a594edd8ed
Le0nX <cloudmindone@gmail.com>
2019-01-07 23:47:08 +0300

    init: initial settings

---
+++ simpe job calc/simpe job calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpe job calc
+//
+//  Created by Denis Nefedov on 07/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
