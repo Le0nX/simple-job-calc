96df20827957a2e981fe6777ef387995daa3ee59
Le0nX <cloudmindone@gmail.com>
2019-01-08 03:30:21 +0300

    feature: added calculate Button

--- simpe job calc/simpe job calc/Views/CurrencyTextField.swift
+++ simpe job calc/simpe job calc/Views/CurrencyTextField.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
 
+    /* We use IBDesgnable to make this code avilable to render
+       our changes in InterfaceBuilder. Also we need to call prepareForInterfaceBuilder()
+       to render out changes */
+    
+    override func prepareForInterfaceBuilder() {
+        customizeButton()
+    }
+    
     /* Will run this code when textfield
         comes up from the interface builder */
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeButton()
+    }
+    
+    func customizeButton(){
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.2525952483)
         layer.cornerRadius = 5.0
         textAlignment = .center
-
+        
         if let p = placeholder {
             let place = NSAttributedString(string: p,
                                            attributes: [.foregroundColor: UIColor.white])
